8cbf5636571e27000375dc70cfca9efb992515fe
Ken <klquicksall@gmail.com>
2012-03-31 20:33:03 -0500

    small change

--- arch/arm/mach-msm/acpuclock-8x60.c
+++ arch/arm/mach-msm/acpuclock-8x60.c
@@ -40,7 +40,7 @@
 #define COMPLEX_SLEW		7
 
 /* PLL calibration limits.
- * The PLL hardware is capable of 192MHz to 1512MHz. The L_VALs
+ * The PLL hardware is capable of 384MHz to 1512MHz. The L_VALs
  * used for calibration should respect these limits. */
 #define L_VAL_SCPLL_CAL_MIN	0x08 /* =  432 MHz with 27MHz source */
 #define L_VAL_SCPLL_CAL_MAX	0x24 /* = 1944 MHz with 27MHz source */
